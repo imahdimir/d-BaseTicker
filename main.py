05d1645c155764c9178d3ac01e702b918e49494e
imahdimir <imahdimir@gmail.com>
2022-08-14 14:38:58 +0430

    + انرژی2

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from pathlib import Path
 
 from mirutil.funcs import save_df_as_a_nice_xl as sxl
 from mirutil.funcs import read_data_according_to_type as rdata
+from mirutil.funcs import norm_fa_str as norm
 
 
 btic = 'BaseTicker'
@@ -28,13 +29,18 @@ def main() :
   ##
   df = rdata(fpn)
   ##
+  df = df.applymap(norm)
+  ##
   df = df.drop_duplicates()
   ##
   df = df.sort_values(btic)
   ##
   sxl(df , fpn)
   ##
-
+  ptr = '\D+'
+  msk = ~ df[btic].str.fullmatch(ptr)
+  df1 = df[msk]
+  ##
   ptr = '\D+'
   msk = ~ df[btic].str.fullmatch(ptr)
   msk &= df[btic].str[:-1].isin(df[btic])
